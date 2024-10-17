b40ff001a836a1f9543aea35b5dfe5e37fc0bcff
swsxm <129607562+swsxm@users.noreply.github.com>
2024-10-17 10:21:28 +0200

    new

--- largest_rectangle_in_histrogram.py
+++ largest_rectangle_in_histrogram.py
@@ -1,37 +1,23 @@
 from typing import List
+
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        stack = []
         rectangle = 0
-        for height in heights:
-            print(stack)
-            pop_count = 0
-            pop_sum = 0
-            while stack and height < stack[-1]:
-                pop_sum += stack.pop()
-                pop_count += 1
-            rectangle = max(pop_sum, rectangle)
-            while pop_count > 0:
-                stack.append(height)
-                pop_count-=1
-            stack.append(height)
-        print('----', stack)
-        while stack:
-            print(stack)
-            last_el = stack.pop()
-            pop_count = 1
-            pop_sum = last_el
-            while stack and last_el == stack[-1]:
-                pop_sum += stack.pop()
-                pop_count += 1
-            rectangle = max(pop_sum, rectangle)
-            while stack and pop_count > 0:
-                stack.append(stack[-1])
-                pop_count-=1
+        stack = []
+        for i, height in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > height:
+                index_of_prev, height_of_prev = stack.pop()
+                rectangle = max(rectangle, height_of_prev * (i - index_of_prev))
+                start = index_of_prev  
+            stack.append((start, height))  
+        
+        for index_of_prev, height_of_prev in stack:
+            rectangle = max(rectangle, height_of_prev * (len(heights) - index_of_prev))
+        
         return rectangle
 
 Test = Solution()
-print(Test.largestRectangleArea(heights = [2,1,5,6,2,3]))
-
+print(Test.largestRectangleArea(heights = [7, 1, 7, 2, 2, 4]))  # Erwartetes Ergebnis: 10
 
         
