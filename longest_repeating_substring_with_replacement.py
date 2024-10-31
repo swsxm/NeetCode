edbf95f80cf843cd0d91d74b5cf42210fe69ca3e
swsxm <129607562+swsxm@users.noreply.github.com>
2024-10-31 22:17:50 +0100

    idk

--- longest_repeating_substring_with_replacement.py
+++ longest_repeating_substring_with_replacement.py
@@ -1,8 +1,19 @@
 class Solution():
     def characterReplacement(self, s: str, k: int) -> int:
-        return max_length
-
+        l = 0
+        count = dict()
+        most_nums = 0
+        longest_seq = 0
+        for i in range(len(s)):
+            count[s[i]] = count.get(s[i], 0) + 1
+            most_nums = max(most_nums, count[s[i]])
+            if (i-l+1) - most_nums > k:
+                count[s[l]]-= 1
+                l+=1
+            print(l, i)
+            longest_seq = max(longest_seq, i - l + 1)
+        return longest_seq
 Test = Solution()
-print(Test.characterReplacement("XYYX", 2))
+print(Test.characterReplacement("AABB", 2))
 
 
