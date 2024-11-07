8a4e0c3928434f140443962ec9e32063e2823fbc
swsxm <129607562+swsxm@users.noreply.github.com>
2024-11-07 23:36:54 +0100

    new

--- copy_linked_list_with_random_pointer.py
+++ copy_linked_list_with_random_pointer.py
@@ -21,6 +21,29 @@ class Solution:
             hashmap[curr].next = hashmap.get(curr.next)
             hashmap[curr].random = hashmap.get(curr.random)
             curr = curr.next
-        return head
-            
+        return hashmap[head] if head else None
 
+            
+    def copyRandomList2(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        curr = head
+        while curr:
+            new = Node(curr.val)
+            new.next = curr.next
+            curr.next = new
+            curr = new.next
+
+        curr = head
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+
+        curr = head
+        head_copy = curr.next
+
+        while curr:
+            tmp = curr.next
+            curr.next = tmp.next
+            curr = curr.next
+        
+        return head_copy
