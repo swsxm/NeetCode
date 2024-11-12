6f8e937f86f2ea57a7cfd739b2c6db904e3bf342
swsxm <129607562+swsxm@users.noreply.github.com>
2024-11-12 21:36:40 +0100

    new

--- repetition/eval_polish_not.py
+++ repetition/eval_polish_not.py
@@ -1,17 +1,18 @@
+from typing import List
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        while len(tokens) > 1:
-            for i in tokens:
-                if i in "-+*/":
-                    val1 = tokens.pop()
-                    val2 = tokens.pop()
-                    if i == "-":
-                        result = val2 - val1
-                    if i == "+":
-                        result = val1 + val2
-                    if i == "*":
-                        result = val1 * val2
-                    if i == "/":
-                        result = val1 * val2
-                    tokens.append(result)
-        return tokens[0]
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
