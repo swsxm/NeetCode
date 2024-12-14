aa890afa043276569b5a67f5f0173f009370a84b
swsxm <129607562+swsxm@users.noreply.github.com>
2024-12-14 14:53:56 +0100

    new

---
+++ repetition/minimum_stack.py
@@ -0,0 +1,21 @@
+class MinStack:
+    def __init__(self):
+        self.min_stack = []
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.min_stack) == 0:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(self.min_stack[-1], val))
+
+    def pop(self) -> None:
+        self.min_stack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
