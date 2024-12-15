f80ededa573a5a2a2c2e9ae23db00f24f6f7881b
swsxm <129607562+swsxm@users.noreply.github.com>
2024-12-15 23:47:25 +0100

    new

---
+++ repetition/evaluate_reverse_polish_notation.py
@@ -0,0 +1,13 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for token in tokens:
+            if token in "+-*/":
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(eval(f"{a}{token}{b}")))
+            else:
+                stack.append(int(token))
+
+        return stack[0]
