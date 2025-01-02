281f9746a3fc1055458026ec1275966591bbe040
swsxm <129607562+swsxm@users.noreply.github.com>
2025-01-02 14:04:04 +0100

    2nd approach

--- valid_binary_search_tree.py
+++ valid_binary_search_tree.py
@@ -18,3 +18,29 @@ class Solution:
             )
 
         return helper(root, float("-inf"), float("inf"))
+
+    def isValidBST2(self, root: Optional[TreeNode]) -> bool:
+        return self.isValid(root)
+
+    def isValid(self, node):
+        if not node:
+            return True
+        if not self.isLeftValid(node.left, node.val):
+            return False
+        if not self.isRightValid(node.right, node.val):
+            return False
+        return self.isValid(node.left) and self.isValid(node.right)
+
+    def isLeftValid(self, node, val):
+        if not node:
+            return True
+        if node.val >= val:
+            return False
+        return self.isLeftValid(node.left, val) and self.isLeftValid(node.right, val)
+
+    def isRightValid(self, node, val):
+        if not node:
+            return True
+        if node.val <= val:
+            return False
+        return self.isRightValid(node.left, val) and self.isRightValid(node.right, val)
