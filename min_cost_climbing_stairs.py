89aed100aad3b9f35eac602724b087f8fbd8d486
swsxm <129607562+swsxm@users.noreply.github.com>
2026-01-10 19:52:19 +0100

    new

---
+++ min_cost_climbing_stairs.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        lookup = {}
+        def dfs(i):
+            if i in lookup:
+                return lookup[i]
+            if i >= len(cost):
+                return 0 
+            res = min(dfs(i+1), dfs(i+2)) + cost[i]
+            lookup[i] = res
+            return res 
+        return min(dfs(0), dfs(1))
+
+test = Solution()
+print(test.minCostClimbingStairs([1,2,3]))
+
+
+
+
+
+
+
+        
+
